532933bcdaa8a56ca1fa00a5482722b73fe6fa81
Graeme simpson <graeme@simmo.gs>
2015-02-25 15:16:40 +0000

    With asset host. Other assets may disappear.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_controller.asset_host = 'https://s3-eu-west-1.amazonaws.com/butterware-asset-test/'
 end
